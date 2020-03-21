93844c191ef9b68a15b47e2cd62de387c66cbd55
Thilara Araujo <thilara.araujo@catho.com>
2020-03-21 13:12:58 -0300

    Template

---
+++ gatsby-starter-interviews/src/components/footer/footer.js
@@ -0,0 +1,18 @@
+// @flow
+import React from 'react';
+
+import './footer.scss';
+
+const Footer = () => {
+  return (
+    <footer id="footer">
+      <div className="bottom-bar">
+        With <span className="heart">♥</span> by <a href="https://www.smooper.com">smooper.com</a>
+        <br/>
+        Solve your digital marketing problems through 1 on 1 consultation with experts
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
